626b84ac2cafe4cb64bf1f3b1daba313d746394a
jvc-github <50381053+jvc-github@users.noreply.github.com>
2022-10-17 12:10:25 -0300

    LIKE (%...), ORDER BY, AVG, NOT

--- celulares_query.sql
+++ celulares_query.sql
@@ -18,3 +18,39 @@ SELECT telefono FROM Usuarios WHERE saldo <= 300;
 
 /* Calcular la suma de los saldos de los usuarios de la compañía telefónica NEXTEL */
 SELECT SUM(saldo) FROM Usuarios WHERE compania = "NEXTEL"
+
+/* Contar el número de usuarios por compañía telefónica */
+SELECT COUNT(usuario) FROM Usuarios GROUP BY compania;
+
+/* Contar el número de usuarios por nivel */
+SELECT COUNT(usuario) FROM Usuarios GROUP BY nivel;
+
+/* Listar el login de los usuarios con nivel 2 */
+SELECT usuario FROM Usuarios WHERE nivel = 2
+
+/* Mostrar el email de los usuarios que usan gmail */
+SELECT email FROM Usuarios WHERE email LIKE "%gmail.com"
+
+/* Listar nombre y teléfono de los usuarios con teléfono LG, SAMSUNG o MOTOROLA */
+SELECT nombre, telefono FROM Usuarios WHERE marca IN("LG", "SAMSUNG", "MOTOROLA");
+
+/* Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca LG o SAMSUNG */
+SELECT nombre, telefono FROM Usuarios WHERE marca NOT IN("LG", "SAMSUNG", "MOTOROLA");
+
+/* Listar el login y teléfono de los usuarios con compañía telefónica IUSACELL */
+SELECT usuario, telefono FROM Usuarios WHERE compania = "IUSACELL";
+
+/* Listar el login y teléfono de los usuarios con compañía telefónica que no sea TELCEL */
+SELECT usuario, telefono FROM Usuarios WHERE compania != "TELCEL";
+
+/* Calcular el saldo promedio de los usuarios que tienen teléfono marca NOKIA */
+SELECT AVG(saldo) FROM Usuarios WHERE marca = "NOKIA";
+
+/* Listar el login y teléfono de los usuarios con compañía telefónica IUSACELL o AXEL */
+SELECT usuario, telefono FROM Usuarios WHERE compania IN("IUSACELL", "AXEL");
+
+/* Mostrar el email de los usuarios que no usan yahoo */
+SELECT email FROM Usuarios WHERE email NOT LIKE ("%yahoo.com");
+
+/* Listar el login y teléfono de los usuarios con compañía telefónica que no sea TELCEL o IUSACELL */
+SELECT usuario, telefono FROM Usuarios WHERE compania NOT IN("TELCEL", "IUSACELL");
